bda15c242f396024606916e941118c59180a5869
lpr <948392651@qq.com>
2025-03-24 16:51:48 +0800

    ok2

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class MyPlugin(BasePlugin):
             # 回复消息 "hello, <发送者id>!"
             # ctx.add_return("reply", [f"id{id}"])
             jm2pdf.download(id)
-            await ctx.send_file(sender_id, f'D:\Documents\Workspace\JmPlugin\data\{id}.pdf')
+            # await ctx.send_file(sender_id, f'D:\Documents\Workspace\JmPlugin\data\{id}.pdf')
 
             # 阻止该事件默认行为（向接口获取回复）
             ctx.prevent_default()
