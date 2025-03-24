305a3808c3bf0c1e6d12e45a56238d33daa250e3
lpr <948392651@qq.com>
2025-03-24 13:51:20 +0800

    ok1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# import jmcomic
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
 from pkg.plugin.events import *  # 导入事件类
+
 
 # 注册插件
 @register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
@@ -33,10 +33,10 @@ class MyPlugin(BasePlugin):
     @handler(GroupNormalMessageReceived)
     async def group_normal_message_received(self, ctx: EventContext):
         msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg[:3] == "hello":  # 如果消息为hello
-            id = int(msg[4:])
+        if msg == "hello":  # 如果消息为hello
+
             # 输出调试信息
-            self.ap.logger.debug(f"id{id}, {ctx.event.sender_id}")
+            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
 
             # 回复消息 "hello, everyone!"
             ctx.add_return("reply", ["hello, everyone!"])
