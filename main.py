57b0582ef9642f75b0571ca0ae76056224445de5
lpr <948392651@qq.com>
2025-03-24 18:19:33 +0800

    ok2

--- main.py
+++ main.py
@@ -69,14 +69,38 @@ class MyPlugin(BasePlugin):
     # 当收到群消息时触发
     @handler(GroupNormalMessageReceived)
     async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
+        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
+        sender_id = ctx.event.sender_id
+        if msg[:3] == "/jm":  # 如果消息为hello
+            id = int(msg[4:])
             # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
+            self.ap.logger.debug(f"id{id}")
 
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
+            # 回复消息 "hello, <发送者id>!"
+            # ctx.add_return("reply", [f"id{id}"])
+            download(id)
+            files_folder = 'D:\Documents\Workspace\JmPlugin\data'
+            file_name = f'{id}.png'
+
+            file_path = None
+            for file in os.listdir(files_folder):
+                if file_name.lower() in file.lower():  # 不区分大小写
+                    file_path = os.path.join(files_folder, file)
+                    break
+            
+            # 检查是否找到文件
+            if file_path and os.path.exists(file_path):
+                try:
+                    # 发送文件
+                    # await ctx.send_file(sender_id, file_path)
+                    file = platform_types.Image(path = file_path)
+                    ctx.add_return('reply', file)
+                    ctx.add_return("reply", ["文件已发送"])
+                except Exception as e:
+                    ctx.add_return("reply", ["发送文件时出错：" + str(e)])
+            else:
+                ctx.add_return("reply", ["未找到匹配的文件"])
+            # await ctx.send_file(sender_id, f'D:\Documents\Workspace\JmPlugin\data\{id}.pdf')
 
             # 阻止该事件默认行为（向接口获取回复）
             ctx.prevent_default()
